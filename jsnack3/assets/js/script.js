1c2619e1290ba231f7bc9af013da0509eb3f91f9
lucafranzoi98 <lucafranzoi98@gmail.com>
2023-08-29 14:23:48 +0200

    jsnack3 completed

---
+++ jsnack3/assets/js/script.js
@@ -0,0 +1,11 @@
+// Il software deve chiedere per 10 volte all’utente di inserire un numero. Il programma stampa la somma di tutti i numeri inseriti.
+
+const sumEl = document.querySelector(".sum");
+let sum = 0;
+
+for (let i = 0; i < 10; i++) {
+   const userNumber = Number(prompt("Inserisci un numero:"));
+   sum += userNumber;
+}
+
+sumEl.innerHTML = sum;
