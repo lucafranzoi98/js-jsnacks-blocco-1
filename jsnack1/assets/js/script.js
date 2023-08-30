702237f61970e75f5e47a52a7e7579326bfa3a9e
lucafranzoi98 <lucafranzoi98@gmail.com>
2023-08-30 10:51:17 +0200

    jscript while

--- jsnack1/assets/js/script.js
+++ jsnack1/assets/js/script.js
@@ -1,6 +1,6 @@
 // L’utente inserisce due numeri in successione, con due prompt. Il software stampa il maggiore.
 
-const firstNumber = Number(prompt("Inserisci il primo numero:"));
+/* const firstNumber = Number(prompt("Inserisci il primo numero:"));
 const secondNumber = Number(prompt("Inserisci il secondo numero:"));
 const resultEl = document.querySelector(".result");
 
@@ -8,6 +8,27 @@ if (firstNumber > secondNumber) {
    resultEl.innerHTML = `<div>${firstNumber}</div>`;
 } else if (firstNumber < secondNumber) {
    resultEl.innerHTML = `<div>${secondNumber}</div>`;
+} else {
+   resultEl.innerHTML = `<div>I numeri sono uguali.</div>`;
+} */
+
+
+let i = 0;
+const numberList = [];
+const resultEl = document.querySelector(".result");
+
+while (i < 2) {
+   const number = Number(prompt("Inserisci un numero:"));
+   numberList.push(number);
+   i++;
+}
+
+console.log(numberList);
+
+if (numberList[0] > numberList[1]) {
+   resultEl.innerHTML = `<div>${numberList[0]}</div>`;
+} else if (numberList[0] < numberList[1]) {
+   resultEl.innerHTML = `<div>${numberList[1]}</div>`;
 } else {
    resultEl.innerHTML = `<div>I numeri sono uguali.</div>`;
 }
